d0c8df6914d2735765e17f29f42fe5faac950224
adrianohenriquess <adriano.henriquesds@gmail.com>
2020-09-06 23:09:43 -0300

    Adicionando authentication via JWT top

--- 09 RestWithSpringBootUdemy Content Negociation/src/main/java/br/com/erudio/config/WebConfig.java
+++ 09 RestWithSpringBootUdemy Content Negociation/src/main/java/br/com/erudio/config/WebConfig.java
@@ -12,45 +12,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-	
+public class WebConfig implements WebMvcConfigurer {
+
 	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
-	
+
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
-	
+
 	public void addCors(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE",
+				"CONNECT");
 	}
-	
+
 	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
-		
-		// Via EXTENSION. localhost:8080/person.x-yaml
-		/*
-		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-		
-		// Via QUERY PARAM. localhost:8080/person?mediaType=xml 
-		/*
-		 * configurer.favorPathExtension(false) .favorParameter(true)
-		 * .parameterName("mediaType") .ignoreAcceptHeader(true)
-		 * .useRegisteredExtensionsOnly(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-		
-		configurer.favorPathExtension(false)
-		.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML)
-		.mediaType("x-yaml", MEDIA_TYPE_YML);
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false)
+				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML)
+				.mediaType("x-yaml", MEDIA_TYPE_YML);
 	}
 
 }
